7402290774160910abc0a4507d1559f6e8861885
Lomudru <108719448+Lomudru@users.noreply.github.com>
2026-03-30 16:28:50 +0200

    Added title and back button

--- frontend/app/parametres.tsx
+++ frontend/app/parametres.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import BackButton from "@/components/BackButton";
+
+const styles = StyleSheet.create({
+  titre : {
+    width: "90%", 
+    height: 50,
+    marginTop: 114, 
+    marginBottom: 50, 
+    alignSelf: "center"
+  },
+  inputPseudo : {
+    width: "80%",
+    height: 50, 
+    backgroundColor: "white",
+    borderWidth: 5, 
+    borderColor: "black", 
+    borderRadius: 100, 
+    paddingHorizontal: 10, 
+    alignSelf: "center", 
+    textAlign: "center",
+  }
+})
 
 export default function Parametres() {
+  const [volume, setVolume] = useState(0.5);
+
   return (
     <View>
-      <Text>Page Paramètres</Text>
+      <BackButton />
+      <Image 
+        source={require('@/assets/images/Parametres_titre.png')}
+        style={styles.titre}
+        resizeMode="contain"
+      />
+      <TextInput 
+        placeholder="Pseudo"
+        style={styles.inputPseudo}
+      />
+      
+
     </View>
   );
 }
